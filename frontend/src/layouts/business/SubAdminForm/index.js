791aac52f82aa42c888582383a4c372315480405
ToqeerFatima <125538720+ToqeerFatima@users.noreply.github.com>
2023-11-19 07:49:21 -0800

    fatima
    subadminform

--- frontend/src/layouts/business/SubAdminForm/index.js
+++ frontend/src/layouts/business/SubAdminForm/index.js
@@ -15,97 +15,128 @@ import SoftTypography from "components/SoftTypography";
 import SoftInput from "components/SoftInput";
 import PageLayout from "examples/LayoutContainers/PageLayout";
 import { Card } from "@mui/material";
-
+import { useState } from "react";
+import axios from "axios";
+import { useSoftUIController } from "../../../context";
 
 function SubAdminForm() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
   const navigate = useNavigate();
-  const businessLogin = () => {
-   
-    navigate('/subAdmins');
-  }
+  const [controller, dispatch] = useSoftUIController();
+  const { baseUrl } = controller;
+
+  const businessLogin = async () => {
+    if (name === "" || email === "") {
+      // Check if email or password is empty
+     alert("Please enter both name and email.");
+      return;
+    }
+
+      // Basic email validation
+      if (!email.includes("@")) {
+        alert("Invalid email format");
+        return;
+      }
+
+      // If validations pass, reset the alert message
+  
+      const formData = new FormData();
+      formData.append("name", name);
+      formData.append("email", email);
+
+      axios({
+        method:" post",
+        url: `${baseUrl}/addSubAdmin`,
+        data: formData,
+        headers: { "Content-Type": "multipart/form-data" },
+      })
+
+      .then(function(response){
+        if (response.data.save == true) {
+          localStorage.setItem('subAdmin', JSON.stringify(response.data.newSubAdmin));
+          navigate('/subAdmins');
+        } else {
+          alert("Enter Correct Email");
+        }
+
+        console.log(response)
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  };
+    // Update the button state when email or password change
+
+  // const businessLogin = () => {
+
+  //   navigate('/subAdmins');
+  // }
   const brand = "";
   return (
     <DashboardLayout>
       <DashboardNavbar />
       <SoftBox
-      width="30vw"
-      height="100%"
-      ml={30}
-      mt={5}
-      // minHeight="100vh"
-    
-      // sx={{ overflowX: "hidden" }}
-    >
-      
-      <Sidenav color="warning" brandName="Food For Each" routes={shopRoutes} />
-      <Card>
-        <SoftBox p={3} mb={1} textAlign="center">
-          <SoftTypography variant="h5" fontWeight="medium">
-            Add New SubAdmin
-          </SoftTypography>
-        </SoftBox>
-        {/* <SoftBox mb={2}>
+        width="30vw"
+        height="100%"
+        ml={30}
+        mt={5}
+        // minHeight="100vh"
+
+        // sx={{ overflowX: "hidden" }}
+      >
+        <Sidenav color="warning" brandName="Food For Each" routes={shopRoutes} />
+        <Card>
+          <SoftBox p={3} mb={1} textAlign="center">
+            <SoftTypography variant="h5" fontWeight="medium">
+              Add New SubAdmin
+            </SoftTypography>
+          </SoftBox>
+          {/* <SoftBox mb={2}>
           <Socials />
         </SoftBox>
         <Separator /> */}
-        <SoftBox pt={2} pb={3} px={3}>
-          <SoftBox component="form" role="form">
-            <SoftBox mb={2}>
-              <SoftInput placeholder="Name" />
-            </SoftBox>
-            <SoftBox mb={2}>
-              <SoftInput type="email" placeholder="Email" />
-            </SoftBox>
-            {/* <SoftBox mb={2}>
-              <SoftInput type="function" placeholder="function" />
-            </SoftBox> */}
-            {/* <SoftBox display="flex" alignItems="center">
-              <Checkbox checked={agreement} onChange={handleSetAgremment} />
-              <SoftTypography
-                variant="button"
-                fontWeight="regular"
-                onClick={handleSetAgremment}
-                sx={{ cursor: "poiner", userSelect: "none" }}
-              >
-                &nbsp;&nbsp;I agree the&nbsp;
-              </SoftTypography>
-              <SoftTypography
-                component="a"
-                href="#"
-                variant="button"
-                fontWeight="bold"
-                textGradient
-              >
-                Terms and Conditions
-              </SoftTypography>
-            </SoftBox> */}
-            <SoftBox mt={4} mb={1}>
-              <SoftButton onClick={()=>{
-            businessLogin();
-          }} variant="gradient" color="warning" fullWidth>
-                Add
-              </SoftButton>
-            </SoftBox>
-            {/* <SoftBox mt={3} textAlign="center">
-              <SoftTypography variant="button" color="text" fontWeight="regular">
-                Already have an account?&nbsp;
-                <SoftTypography
-                  component={Link}
-                  to="/authentication/sign-in"
-                  variant="button"
-                  color="dark"
-                  fontWeight="bold"
-                  textGradient
+          <SoftBox pt={2} pb={3} px={3}>
+            <SoftBox component="form" role="form">
+              <SoftBox mb={2}>
+                <SoftInput
+                  name="name"
+                  placeholder="Name"
+                  onChange={(e) => {
+                    setName(e.target.value);
+                  }}
+                  value={name}
+
+                />
+              </SoftBox>
+              <SoftBox mb={2}>
+                <SoftInput
+                  required
+                  type="email"
+                  placeholder="Email"
+                  onChange={(e) => {
+                    setEmail(e.target.value);
+                  }}
+                  value={email}
+
+                />
+              </SoftBox>
+              <SoftBox mt={4} mb={1}>
+                <SoftButton
+                  onClick={() => {
+                    businessLogin();
+                  }}
+                  variant="gradient"
+                  color="warning"
+                  fullWidth
                 >
-                  Sign in
-                </SoftTypography>
-              </SoftTypography>
-            </SoftBox> */}
+                  Add
+                </SoftButton>
+              </SoftBox>
+            </SoftBox>
           </SoftBox>
-        </SoftBox>
-      </Card>
-      <SoftBox py={4}></SoftBox>
-     
+        </Card>
+        <SoftBox py={4}></SoftBox>
       </SoftBox>
       <Footer />
     </DashboardLayout>
